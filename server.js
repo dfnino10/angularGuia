2e0837fabba175c7f7b74b894587a65ce0898ae1
Camilo Ortiz <ca.ortiz1633@uniandes.edu.co>
2019-09-02 02:01:31 -0500

    Server updated

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/guiacolombianafront'));
+app.use(express.static(__dirname + '/dist/GuiaColombiAng'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/guiacolombianafront/index.html'));
+res.sendFile(path.join(__dirname+'/dist/GuiaColombiAng/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
