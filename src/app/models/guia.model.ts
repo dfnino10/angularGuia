f0a43f6f37fb3550a6d8a218c65b50748ca64a3c
Eduard <eduard.duarte@hotmail.com>
2019-09-03 14:25:38 -0500

    dashboard guias

--- src/app/models/guia.model.ts
+++ src/app/models/guia.model.ts
@@ -1,5 +1,5 @@
 
-export class Tour {
+export class Guia {
   apellidoPaterno: string; //CharField(max_length=35)
   apellidoMaterno: string; //CharField(max_length=35)
   nombres: string; //CharField(max_length=35)
@@ -7,12 +7,17 @@ export class Tour {
   fechaNacimiento: Date; //models.DateField()
   sexo: string; //SEXOS = (('F', 'Femenino'),('M', 'Masculino'))
   descripcion: string; //models.CharField(max_length=200)
-  categoria: string; //
+  // una  frase debajo del nombre
+  categorias: string[]; // Listado de categorias q aplican para el Guia
+  //faltan
+  fotoUrl: string; //foto de algún citio de la ciudad
+  usuariosRedes: string; //(Facebook, Twitter, Instagram)
+
 }
 /*
 CATEGORIAS = (
   ('Mu', 'Museos'),
-  ('Re', 'Restaurantes'), 
+  ('Re', 'Restaurantes'),
   ('Bic', 'Paseos de Bicileta'),
   ('SO', 'Sitios ocultos')
 )
--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Guia } from '../../models/guia.model'
 
 @Component({
   selector: 'app-dashboard',
@@ -14,6 +15,37 @@ export class DashboardComponent implements OnInit {
   categorys: any[] = [
     { value: 'steak-0', viewValue: 'Extreme' },
     { value: 'pizza-1', viewValue: 'fly' }
+  ];
+
+  Guias: Guia[] = [
+    {
+      apellidoPaterno: 'Duarte',
+      apellidoMaterno: 'Sepúlveda',
+      nombres: "Eduard",
+      documento: '1090366576',
+      fechaNacimiento: new Date(1992, 2, 14),
+      sexo: 'M',
+      descripcion: 'Experto en Deportes extremos',
+      // una  frase debajo del nombre
+      categorias: ['Deportes','canoping'], //ojo debe ser una lista
+      //faltan
+      fotoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1CHZcIamB4hIA-AsPIC9eyBeC-WK17CG4PICi3K_pnqhwtiGF',
+      usuariosRedes: 'https://github.com/EduarDuarteS',
+    },
+    {
+      apellidoPaterno: 'Gomez',
+      apellidoMaterno: 'Tobeda',
+      nombres: "Camila",
+      documento: '1648526',
+      fechaNacimiento: new Date(1996, 5, 8),
+      sexo: 'F',
+      descripcion: 'Vive la aventura',
+      // una  frase debajo del nombre
+      categorias: ['DE'], //ojo debe ser una lista
+      //faltan
+      fotoUrl: 'https://cdn.colombia.com/sdi/2019/03/23/las-mujeres-de-bogota-se-toman-los-deportes-extremos-sobre-ruedas-721813.jpg',
+      usuariosRedes: 'https://www.facebook.com/people/Camila/100037474220943',
+    }
   ];
   constructor() { }
 
