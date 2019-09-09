6034798d86678f1b7d52a11a090becca78f845f6
Eduard <eduard.duarte@hotmail.com>
2019-09-08 21:28:32 -0500

    espacio

--- src/app/services/service.index.ts
+++ src/app/services/service.index.ts
@@ -1,6 +1,7 @@
 // Guards
 export { LoginGuardGuard } from './guards/login-guard.guard';
 export { AdminGuard } from './guards/admin.guard';
+
 export { GuiasService } from './guias/guias.service'
 export { UsuarioService } from './usuario/usuario.service';
 export { SettingsService } from './settings/settings.service';
